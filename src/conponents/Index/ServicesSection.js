86772a47b61a1a391f1f2bb4bdcfe062159e4621
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-16 15:04:22 +0700

    [UPDATE] Add text

--- src/conponents/Index/ServicesSection.js
+++ src/conponents/Index/ServicesSection.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Landing = styled.div`
+    margin-bottom:70em;
+`;
 const MainServicesSection = ()=>(
-
+    <Landing>
+    <h3>OUR MAIN SERVICES</h3>
+    <h5>LET ME INTRODUCE US</h5>
+    <p>Vestibulum ante ipsum primis in faucibus orci
+         luctus et ultrices posuere cubilia Curae; Fusce
+         id purus. Ut varius tincidunt libero. Phasellus dolor.
+         Maecenas vestibulum es mollis diam. Pellentesque ut neque.
+        Pellentesque habitant morbi tristique senectus et netus et
+        malesuada fames ac turpis egestas. In dui magna, posuere eget,
+              vestibulum, tempor auctor, justo. In ac felis quis tortor
+               malesuada pretium. Pellentesque auctor neque nec urna. Proin sapien 
+               ipsum, porta, euismod ut, mi. Aenean viverra rhoncus pede.
+                Pellentesque habitant morbi tristique senectus et netus et malesuada 
+                fames ac turpis egestas.
+                </p>
+    </Landing>
 );
 
 export default MainServicesSection;
