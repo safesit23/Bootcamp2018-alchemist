c4c73c96c4ec52dea83de59958bd664f201a0e21
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-16 16:33:50 +0700

    [UPDATE] add Section to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component,Fragment } from 'react';
 import './App.css';
 import Navbar from './conponents/Index/Navbar';
 import ToptextSection from './conponents/Index/ToptextSection';
+import ServicesSection from './conponents/Index/ServicesSection';
+import ProjectSection from './conponents/Index/ProjectSection';
+import Footer from './conponents/Index/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
       <Navbar/>
       <ToptextSection/>
-      </div>
+      <ServicesSection/>
+      <ProjectSection/>
+      <Footer />
+      </Fragment>
     );
   }
 }
