f62d9edab2315a874765633f253501b3b5ab68d3
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-16 16:32:00 +0700

    [ADD] FooterSection

---
+++ src/conponents/Index/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = ()=>(
+    <div>
+        <img src='/static/images/social-buttons-sprite.png' alt='social'/>
+        <img src='/static/images/footerBG.png' alt='footer'/>
+    </div>
+);
+
+export default Footer;
