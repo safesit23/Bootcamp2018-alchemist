1cd36beb0adeed56b1ed436c0e0cc7e2bd91645c
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-16 11:41:36 +0700

    [UPDATE] Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './conponents/Navbar';
+import Navbar from './conponents/Index/Navbar';
+import ToptextSection from './conponents/Index/ToptextSection';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Navbar/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <Navbar/>
+      <ToptextSection/>
       </div>
     );
   }
