60e560bcefe41dd2bee45336f0889c8238d41959
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-16 14:59:55 +0700

    [UPDATE] Toptext Section

--- src/conponents/Index/ToptextSection.js
+++ src/conponents/Index/ToptextSection.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React,{Fragment} from 'react';
+import {Container} from 'reactstrap';
+import styled from 'styled-components';
 
+const slideElement = ['WEB & APP', 'BRANDING', 'CARTOONS'];
+
+const Landing = styled.div`
+    padding: 200px 0;
+`;
+
+const Topic = styled.h1`
+  color: white;
+  text-align: center;
+  font-size: 5em;
+`;
 class ToptextSection extends React.Component{
     render(){
         return(
-            <h1>I LOVE TO MAKE</h1>
+            <Fragment>
+                <Container>
+                <Landing >
+                    <Topic>I LOVE TO MAKE</Topic>
+                    <Topic>{slideElement[0]}</Topic>
+                    <Topic>LOOK AWESOME</Topic>
+                </Landing>
+                </Container>
+            </Fragment>
         );
     }
 }
