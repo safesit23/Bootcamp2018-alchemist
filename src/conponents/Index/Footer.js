1f57d401d0b6ec2006b0fa6ed17fc9907a5b2321
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-16 16:50:26 +0700

    [UPDATE] add styled

--- src/conponents/Index/Footer.js
+++ src/conponents/Index/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import styled from 'styled-components';
+const Header = styled.h2`
+text-align: center;
+`;
 const Footer = ()=>(
     <div>
+        <Header>WE ARE SOCIAL!</Header>
+        <div className='d-flex justify-content-center'>
         <img src='/static/images/social-buttons-sprite.png' alt='social'/>
+        </div>
         <img src='/static/images/footerBG.png' alt='footer'/>
     </div>
 );
