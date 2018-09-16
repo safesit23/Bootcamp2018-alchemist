9f110b1a80bff20d6829bdd1e168e672f41b9242
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-16 11:41:20 +0700

    [ADD] ToptextSection comp

---
+++ src/conponents/Index/ToptextSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class ToptextSection extends React.Component{
+    render(){
+        return(
+            <h1>I LOVE TO MAKE</h1>
+        );
+    }
+}
+export default ToptextSection;
