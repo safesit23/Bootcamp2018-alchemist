e29604c5cc1c01a45e3afa839f5b189bbd3247e4
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-16 16:32:27 +0700

    [UPDATE] add Image

--- src/conponents/Index/ProjectSection.js
+++ src/conponents/Index/ProjectSection.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import {Container, Row, Col} from 'reactstrap';
+
+const Landing = styled.div`
+    margin-bottom: 40em;
+`;
+
+const Header = styled.h1`
+    text-align: center;
+`;
+
+const Image = styled.img`
+    border: 5px solid white;
+`;
+
+const imagesrc = 'static/images/1.jpg';
 const ProjectSection = ()=>(
-<div>
-    <h3>RECENT PROJECT</h3>
-</div>
+<Landing>
+    <Header>RECENT PROJECT</Header>
+    <Container >
+    <Row>
+        {
+            [1,2,3,4,5,6].map(value => (
+                <Col lg={4} className='d-flex justify-content-center'>
+                    <Image className='mx-2' src={imagesrc} alt='pic' />
+                </Col>
+            ))
+        }
+        
+    </Row>
+    </Container>
+</Landing>
 );
 
 export default ProjectSection;
